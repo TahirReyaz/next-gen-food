027c9490e22c4aac3e84878c81b496f3e40ff531
TahirReyaz <tahirreyaz5@gmail.com>
2024-04-01 17:28:48 +0530

    Fixed image importing on details page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'maxschwarzmueller-nextjs-demo-users-image.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
